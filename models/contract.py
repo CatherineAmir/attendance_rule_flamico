5783ed53a364ad01334053d48c42d67be66ca1ce
CatherineAmir <catherine.amir20@gmail.com>
2026-07-15 15:24:59 +0300

    make contract data stored

--- models/contract.py
+++ models/contract.py
@@ -4,24 +4,24 @@ from odoo import fields, models, api
 class Contract(models.Model):
     _inherit = 'hr.contract'
 
-    take_overtime = fields.Boolean(string="Take Overtime",default=False,store=True)
+    take_overtime = fields.Boolean(string="Take Overtime",default=False,store=True,tracking=True)
     # apply_lateness=fields.Boolean(string="Apply Lateness",default=False,store=True)
     lateness_policy = fields.Selection([
         ("no","No Lateness Deduction"),
         ("apply_lateness_rules","Apply Lateness Rules Deduction"),
         ('apply_lateness_hourly_quarter','Apply Lateness Hourly Deduction'),
-    ],default='no',string='Lateness Deduction Policy')
-    apply_early_leaving=fields.Boolean(string="Apply Early Leaving Deduction (Hourly)",default=False,store=True)
+    ],default='no',string='Lateness Deduction Policy',tracking=True)
+    apply_early_leaving=fields.Boolean(string="Apply Early Leaving Deduction (Hourly)",default=False,store=True,tracking=True)
     absence=fields.Selection([
         ("no","No Deduction"),
         ("day_day","Day by day"),
         ("day_by_day_half","Day by day and half"),
-    ],default='day_by_day_half')
-    work_with_attendance = fields.Boolean(string="Work With Attendance",store=True)
-    daily_rate = fields.Float(string="Daily Rate", compute='_compute_daily_rate', store=True)
-    hourly_rate = fields.Float(string="Hourly Rate", compute='_compute_hourly_rate', store=True)
-    bonus_public_holiday = fields.Float(string="Bonus Public Holiday",default=0,store=True)
-    overtime_hourly_rate = fields.Float(string="Overtime Hourly Rate")
+    ],default='day_by_day_half',tracking=True)
+    work_with_attendance = fields.Boolean(string="Work With Attendance",store=True,tracking=True)
+    daily_rate = fields.Float(string="Daily Rate", compute='_compute_daily_rate', store=True,tracking=True)
+    hourly_rate = fields.Float(string="Hourly Rate", compute='_compute_hourly_rate', store=True,tracking=True)
+    bonus_public_holiday = fields.Float(string="Bonus Public Holiday",default=0,store=True,tracking=True)
+    overtime_hourly_rate = fields.Float(string="Overtime Hourly Rate",tracking=True)
     @api.depends("wage")
     def _compute_daily_rate(self):
         for r in self:
